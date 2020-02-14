2fae04f59b63266476402fb59ac744b95d19a280
jojiiOfficial <jojii@jojii.de>
2020-02-14 15:36:53 +0100

    add structs and queries

--- Queries.go
+++ Queries.go
@@ -6,8 +6,18 @@ import (
 
 func getInitSQL() dbhelper.QueryChain {
 	return dbhelper.QueryChain{
-		Name:    "initChain",
-		Order:   0,
-		Queries: dbhelper.CreateInitVersionSQL(),
+		Name:  "initChain",
+		Order: 0,
+		Queries: dbhelper.CreateInitVersionSQL(
+			dbhelper.InitSQL{
+				Query: "CREATE TABLE Sources (pk_id int(10) UNSIGNED NOT NULL, creator int(10) UNSIGNED NOT NULL, name text NOT NULL, secret varchar(48) NOT NULL, creationTime int(11) NOT NULL, private tinyint(1) NOT NULL DEFAULT '0' ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4",
+			},
+			dbhelper.InitSQL{
+				Query: "ALTER TABLE Sources ADD PRIMARY KEY (pk_id)",
+			},
+			dbhelper.InitSQL{
+				Query: "ALTER TABLE Sources MODIFY pk_id int(10) UNSIGNED NOT NULL AUTO_INCREMENT",
+			},
+		),
 	}
 }
--- Structs.go
+++ Structs.go
@@ -6,9 +6,19 @@ import "time"
 
 //User user in db
 type User struct {
-	Pkid      int       `db:"pk_id"`
+	Pkid      int       `db:"pk_id" orm:"pk,ai"`
 	Username  string    `db:"username"`
-	Token     string    `db:"token"`
 	CreatedAt time.Time `db:"createdAt"`
 	IsValid   bool      `db:"isValid"`
+}
+
+//Source a webhook source
+type Source struct {
+	PkID         uint32 `db:"pk_id" orm:"pk,ai"`
+	CreatorID    uint32 `db:"creator"`
+	Creator      User   `db:"-" orm:"-"`
+	Name         string `db:"name"`
+	Secret       string `db:"secret"`
+	CreationTime string `db:"creationTime"`
+	IsPrivate    bool   `db:"private"`
 }
