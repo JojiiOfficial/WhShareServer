77e7f77117460a40cc93eb64ef38f30298d106fe
jojiiOfficial <jojii@jojii.de>
2020-02-23 12:40:19 +0100

    moved enVarUtils to main.go

--- main.go
+++ main.go
@@ -45,6 +45,22 @@ var (
 	isDebug bool = false
 )
 
+//Env vars
+const (
+	//EnVarPrefix prefix of all used env vars
+	EnVarPrefix = "S"
+
+	EnVarLogLevel   = "LOG_LEVEL"
+	EnVarNoColor    = "NO_COLOR"
+	EnVarYes        = "SKIP_CONFIRM"
+	EnVarConfigFile = "CONFIG"
+)
+
+//Return the variable using the server prefix
+func getEnVar(name string) string {
+	return fmt.Sprintf("%s_%s", EnVarPrefix, name)
+}
+
 func main() {
 	//Set app attributes
 	app.HelpFlag.Short('h')
